f873d55bcb44d4c42376f0c583b73f7380fe9ba8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-08-27 19:20:58 +0200

    Update VS Code settings to use Prettier as the default formatter and enhance hover provider for Ink syntax

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { CancellationToken, ExtensionContext, Hover, HoverProvider, languages, Position, TextDocument } from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+    context.subscriptions.push(
+        languages.registerHoverProvider("ink", <HoverProvider>{
+            provideHover(document: TextDocument, position: Position, token: CancellationToken) {
+                const range = document.getWordRangeAtPosition(position, /\b(END|DONE)\b/);
+                if (!range) {
+                    return undefined;
+                }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pixi-vn-ink-vscode" is now active!');
+                const word = document.getText(range);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('pixi-vn-ink-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from !');
-	});
+                if (word === "END") {
+                    return new Hover(
+                        "**END**: Ends the current story flow immediately. Use this when the story should stop completely."
+                    );
+                }
 
-	context.subscriptions.push(disposable);
+                if (word === "DONE") {
+                    return new Hover(
+                        "**DONE**: Marks the current knot as finished. The story flow can continue to the next knot or choice."
+                    );
+                }
+
+                return undefined;
+            },
+        })
+    );
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() { }
